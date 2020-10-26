3a406728e8648b2078c3056b2c3a975d73e5fbe9
sangramga <sangramga@gmail.com>
2020-10-27 02:14:44 +0530

    Added delete() method in blog.py with some bug fixes

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -44,7 +44,7 @@ def get_post(id, check_author=True):
     """ GET posts from db by id and check login user id is post author_id"""
     db = get_db()
     post = db.execute(
-        "SELECT p.id, title, author, created, author_id, username"
+        "SELECT p.id, title, body, created, author_id, username"
         " FROM post p JOIN user u ON p.author_id = u.id"
         " WHERE p.id = ?",(id, )
         ).fetchone()
@@ -60,7 +60,7 @@ def update(id):
     post = get_post(id)
     if request.method == "POST":
         title = request.form['title']
-        body = request.body['body']
+        body = request.form['body']
         error = None
         db = get_db()
         if not title:
@@ -77,3 +77,12 @@ def update(id):
             return redirect(url_for('blog.index'))
 
     return render_template("blog/update.html",post=post)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
