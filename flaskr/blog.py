1e78eab145022f23518fed3caaea25fb74fad0a1
sangramga <sangramga@gmail.com>
2020-10-27 01:00:09 +0530

    Added /create route in blog.py and create.html template

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -17,3 +17,26 @@ def index():
     " ORDER BY created DESC"
     ).fetchall()
     return render_template('blog/index.html',posts=posts)
+
+@bp.route("/create",methods = ("GET","POST"))
+@login_required
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+        db = get_db()
+
+        if not title:
+            error = "title is required"
+        if error is not None:
+            flash(error)
+        
+        db.execute(
+            "INSERT INTO post (title, body, author_id)"
+            " VALUES (?, ?, ?)",(title, body, g.user['id'])
+        )
+        db.commit()
+        return redirect(url_for('blog.index'))
+    return render_template('blog/create.html')
+
