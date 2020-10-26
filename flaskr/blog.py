596fc69900cc534b8fc36d6ae3dba4b5edea3c6b
sangramga <sangramga@gmail.com>
2020-10-27 01:21:16 +0530

    Added get_post() by id and update for blog blueprint and template for update.html

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -13,7 +13,7 @@ def index():
     db = get_db()
     posts = db.execute(
     "SELECT p.id, title, body, created, author_id, username"
-    " FROM post p JOIN user u WHERE p.author_id = u.id"
+    " FROM post p JOIN user u ON p.author_id = u.id"
     " ORDER BY created DESC"
     ).fetchall()
     return render_template('blog/index.html',posts=posts)
@@ -40,3 +40,40 @@ def create():
         return redirect(url_for('blog.index'))
     return render_template('blog/create.html')
 
+def get_post(id, check_author=True):
+    """ GET posts from db by id and check login user id is post author_id"""
+    db = get_db()
+    post = db.execute(
+        "SELECT p.id, title, author, created, author_id, username"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        " WHERE p.id = ?",(id, )
+        ).fetchone()
+    if post is None:
+        abort(404, "Post id {0} doesn't exist.".format(id))
+    if check_author and post["author_id"] != g.user['id']:
+        abort(403)
+    return post
+        
+@bp.route("/<int:id>/update", methods=("GET","POST"))
+@login_required
+def update(id):
+    post = get_post(id)
+    if request.method == "POST":
+        title = request.form['title']
+        body = request.body['body']
+        error = None
+        db = get_db()
+        if not title:
+            error = "Title is required"
+        if error is not None:
+            flash(error)
+        else:
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template("blog/update.html",post=post)
